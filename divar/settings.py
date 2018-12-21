fa287bc1f290207c1eaf7e818b29cbcd7f377e2d
hossein76rahimi <hossein76rahimi@gmail.com>
2018-12-21 18:51:13 +0330

    add:all methods added

--- divar/settings.py
+++ divar/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '0l+mi)rq)ano40e1%stjj82$ze)2ee05i=#ar6!$5abf@!!&38'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -71,17 +69,18 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'divar.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "divar",
+        "HOST": "localhost",
+        "USERNAME": "hossein",
+        "PASSWORD": "abcd@1234"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -101,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
@@ -114,7 +112,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
--- product/models.py
+++ product/models.py
@@ -1,16 +1,133 @@
 from django.db import models
+from django_mysql.models import JSONField
 
 
+# mappers
+
+# city vicinity mapper
+class CVMapper(models.Model):
+    city = models.ForeignKey("City", on_delete=models.CASCADE)
+    vicinities = models.ManyToManyField("Vicinity")
+
+    def add_vicinity(self, vic):
+        if not isinstance(vic, Vicinity):
+            raise Exception
+        self.vicinities.add(vic)
+        self.save()
+
+    def get_vicinities(self):
+        return self.vicinities
+
+    def delete_vicinity(self, vic):
+        if not isinstance(vic, Vicinity) or not vic in self.vicinities:
+            raise Exception
+        self.vicinities.remove(vic)
+
+
+# category product mapper
+class CPMapper(models.Model):
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    products = models.ManyToManyField("Product")
+
+    def add_product(self, product):
+        if not isinstance(product, Product):
+            raise Exception
+        self.products.add(product)
+        self.save()
+
+    def get_products(self):
+        return self.products
+
+    def delete_product(self, product):
+        if not isinstance(product, Product) or not product in self.products:
+            raise Exception
+
+        self.products.remove(product)
+        self.save()
+
+
+# Vicinity Product mapper
+class VPMapper(models.Model):
+    vicinity = models.ForeignKey("Vicinity", unique=True, on_delete=models.CASCADE)
+    products = models.ManyToManyField("Product")
+
+    # void
+    def add_product(self, product):
+        if not isinstance(product, Product):
+            raise Exception
+        self.products.add(product)
+        self.save()
+
+    # list
+    def get_products(self):
+        return self.products
+
+    # void
+    def delete_product(self, product):
+        if not isinstance(product, Product) or not product in self.products:
+            raise Exception
+
+        self.products.remove(product)
+        self.save()
+
+
+# models
 class City(models.Model):
     name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        super(City, self).save(*args, **kwargs)
+        CVMapper.objects.get_object_or_create(city=self)
+
+    def get_vicinities(self):
+        cmap = CVMapper.objects.get(city=self)
+        return cmap.get_vicinities()
+
+    def get_products(self):
+        vics = self.get_vicinities()
+        p = Product.objects.none()
+        for v in vics:
+            p = (p | v.get_products()).distinct()
+        return p
+
 
 class Vicinity(models.Model):
     name = models.CharField(max_length=255)
     city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        super(Vicinity, self).save(*args, **kwargs)
+        VPMapper.objects.get_or_create(vicinity=self)
+        cv = CVMapper.objects.get_object_or_create(city=self.city)
+        cv.add_vicinity(vic=self)
+
+    def get_products(self):
+        cmap = VPMapper.objects.get(vicinity=self)
+        return cmap.get_products()
+
+    def add_product(self, product):
+        cmap = VPMapper.objects.get(vicinity=self)
+        if not isinstance(product, Product):
+            raise Exception
+        cmap.add_product(product)
+        cmap.save()
+
+    def delete(self, *args, **kwargs):
+        cmap = VPMapper.objects.get(vicinity=self)
+        cmap.delete()
+        super(Vicinity, self).delete(*args, **kwargs)
+
+    def delete_product(self, product):
+        if not isinstance(product, Product):
+            raise Exception
+        cmap = VPMapper.objects.get(vicinity=self)
+        cmap.delete_product(product)
+
+    def get_city(self):
+        return self.city
 
 
 class ProductImage(models.Model):
@@ -24,40 +141,58 @@ class AttributeChoiceValue(models.Model):
 
 class Attribute(models.Model):
     name = models.CharField(max_length=100)
-    valid_values = models.ManyToManyField(AttributeChoiceValue)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     parent = models.ForeignKey('self', related_name="childs", on_delete=models.CASCADE)
-    products = models.ManyToManyField('Product')
+
+    def save(self, *args, **kwargs):
+        super(Category, self).save(*args, **kwargs)
+        CPMapper.objects.get_or_create(category=self)
+
+    def get_product(self):
+        cmap = CPMapper.objects.get(category=self)
+        return cmap.get_products()
+
+    def add_product(self, product):
+        cp = CPMapper.objects.get_or_create(category=self)
+        if not isinstance(product, Product):
+            raise Exception
+        cp.add_product(product)
+        cp.save()
+
+    def delete(self, *args, **kwargs):
+        cmap = CPMapper.objects.get(category=self)
+        cmap.delete()
+        super(Category, self).delete(*args, **kwargs)
+
+    def delete_product(self, product):
+        if not isinstance(product, Product):
+            raise Exception
+        cmap = CPMapper.objects.get(category=self)
+        cmap.delete_product(product)
+
+    def get_parent(self):
+        return self.parent
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.FloatField()
     image = models.ManyToManyField(ProductImage)
     vicinity = models.ForeignKey(Vicinity, on_delete=models.CASCADE)
-    attributes = models.ManyToManyField(Attribute)
-    creation_time = models.DateTimeField(default=None, null=True, blank=True)
+    attributes = JSONField(default=dict)
+    creation_time = models.DateTimeField(auto_now=True, null=True, blank=True)
 
+    def save(self, *args, **kwargs):
+        super(Product, self).save(*args, **kwargs)
+        self.vicinity.add_product(self)
+        self.category.add_product(self)
 
-# mappers
-
-# city vicinity mapper
-class CVMapper(models.Model):
-    city = models.ForeignKey("City", on_delete=models.CASCADE)
-    vicinities = models.ManyToManyField("Vicinity")
-
-
-# category product mapper
-class CPMapper(models.Model):
-    category = models.ForeignKey("Category", on_delete=models.CASCADE)
-    products = models.ManyToManyField("Product")
-
-
-# Vicinity Product mapper
-class VPMapper(models.Model):
-    vicinity = models.ForeignKey("Vicinity", unique=True, on_delete=models.CASCADE)
-    products = models.ManyToManyField("Product")
+    def delete(self, *args, **kwargs):
+        self.category.delete_product(self)
+        self.vicinity.delete_product(self)
+        super(Product, self).delete(*args, **kwargs)
